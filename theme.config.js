8c2391e0267cab2ddce9d75ee001a0e5b09c288d
koji-tanibe <kojitanibe1103@gmail.com>
2022-01-08 00:52:36 +0900

    first commit

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      {/* <time>{YEAR}</time> © Koji Tanibe.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
@@ -15,7 +15,7 @@ export default {
             padding-bottom: 4rem;
           }
         }
-      `}</style>
+      `}</style> */}
     </small>
   )
 }
